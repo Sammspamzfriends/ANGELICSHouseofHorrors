71be36fdc0fa6da8ecdf5e482c9ee917eb44b9e8
Sam <161511568+Sammspamzfriends@users.noreply.github.com>
2025-05-09 14:54:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+function animateGlossophobia () {
+	
+}
+function setupArachnophobia () {
+	
+}
+function setupScopophobia () {
+	
+}
+function setupGlossophobia () {
+	
+}
+function spScopophobiaEyes () {
+	
+}
+function setupNyctophobia () {
+	
+}
+function spArachnophobiaSpiders () {
+	
+}
+function setupAstraphobia () {
+	
+}
+function setupApeirophobia () {
+	
+}
+function animateNyctophobia () {
+	
+}
+function SpawnMC () {
+	
+}
+function animateAstraphobiaLightning () {
+	
+}
